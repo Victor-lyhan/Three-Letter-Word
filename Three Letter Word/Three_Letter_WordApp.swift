0f745310ccbd3d3e2f487dbd0aaa124410149553
Victor Han <liyanghan1112@gmail.com>
2022-07-22 09:52:53 -0500

    Initial Commit

---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by Victor Han on 2022/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
